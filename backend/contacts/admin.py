e35e9dc723f076276a7025a2e843cb4347764545
alexnick92 <alex.nicholas92@gmail.com>
2021-11-03 11:17:47 +0800

    defined contacts model & migrated

--- backend/contacts/admin.py
+++ backend/contacts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import contacts
+
+class contactsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'number')
 
 # Register your models here.
+
+admin.site.register(contacts, contactsAdmin)
--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class contacts(models.Model):
+    name = models.CharField(max_length=120)
+    number = models.CharField(max_length=15)
+    
+    def _str_(self):
+        return self.title
